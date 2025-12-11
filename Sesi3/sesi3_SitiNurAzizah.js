68e97478433a233232455e2b46fe8f7fccdb352d
Azizah <90896327+sukasukajija@users.noreply.github.com>
2025-12-11 07:48:54 +0700

    loop statement to make segitiga sesi 3

--- Sesi3/sesi3_SitiNurAzizah.js
+++ Sesi3/sesi3_SitiNurAzizah.js
@@ -1,1 +1,15 @@
-// if else, try catche, while for
+// Buatlah segitia asterik dengan menggunakan loop statement 
+
+function buatSegitigaAsterik(baris) {
+    let hasil = '';
+    for (let i = 1; i <= baris; i++) {
+        for (let j = 0; j < i; j++) {
+            hasil += '*';
+        }
+        hasil += '\n';
+    }
+    return hasil;
+}
+
+// Test dengan 3 baris
+console.log(buatSegitigaAsterik(3));
